be067519f04bb3b3bd0f257f7b2e015ba4202a7b
Lkhagva-erdene <anidas.com@gmail.com>
2021-02-22 00:44:35 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int age = 12;
+	int y = 10;
+	
+	cout<<"Burd's age is ";
+	cout<<age;
+	
+	
+	return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x = 12;	//declare 3 variable add those 3 number
+	int y = 10; //subtract from x - y - z
+				//multiply x*y*z
+				// divition operation on x/y
+				// x + y/z - z
+	
+	cout<<"x + y = ";
+	cout<<x + y;
+	
+	
+	return 0;
+}
